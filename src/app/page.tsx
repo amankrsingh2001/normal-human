27bc59c628b9e8ca381f70eefbe38b6f655ca769
Amansingh <singhamankr17@gmail.com>
2024-12-31 23:36:48 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default async function Home() {
+
+  return (
+    <>
+     <h1 className="text-red-500">Hello world</h1>
+     <Button>This is the button</Button>
+    </>
+     
+  );
+}
