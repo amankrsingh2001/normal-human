1d5147a6e906b397d1c95798c0135fcd4ce61837
Amansingh <singhamankr17@gmail.com>
2025-01-03 15:35:59 +0530

    ignore typeerror

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -2,7 +2,6 @@ import { db } from "@/server/db"
 
 export const POST = async(req:Request)=>{
     const {data} = await req.json()
-    console.log(data,"THIS IS THE DATA")
     const emailAddress = data.email_addresses[0].email_address
     const firstName = data.first_name
     const lastName = data.last_name
