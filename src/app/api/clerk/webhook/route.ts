1d0c2c9a8fd08a815db33e358f90402caf6375a1
Amansingh <singhamankr17@gmail.com>
2025-01-07 16:09:19 +0530

    create user and env example

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,9 +1,24 @@
+import { db } from "@/server/db";
 import { NextRequest } from "next/server";
 
 export const POST = async(req:NextRequest)=>{
+
     const {data} = await req.json()
     console.log(data)
-    return new Response(
-        "Data recieved",{status:200}
-    )
+    const emailAddress = data.email_addresses[0].email_address
+    const firstName = data.first_name
+    const lastName = data.last_name
+    const imageUrl = data.image_url
+
+    await db.user.create({
+        data:{
+            emailAddress:emailAddress,
+            firstName:firstName,
+            lastName:lastName,
+            imageUrl:imageUrl
+        }
+    })
+    
+    return new Response("webhook working", {status:200})
+    
 }
